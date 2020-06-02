72abc9a84971874f38531139bb3c8b06e02adc61
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-06-02 21:14:05 +0200

    added buttoncomponents

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { cart } from "reducers/cart";
 import { products } from "reducers/products";
 import { user } from "reducers/user";
+import { Button } from "components/Button";
 
 const reducer = combineReducers({
   cart: cart.reducer,
@@ -16,7 +17,8 @@ const store = configureStore({ reducer });
 export const App = () => {
   return (
     <Provider store={store}>
-      <div>Find me in src/app.js! no?</div>
+      <div>Find me in src/app.js! </div>
+      <Button />
     </Provider>
   );
 };
