6e2702411f860aaf773c01295fe4f9a5d240797f
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-06-03 10:50:56 +0200

    added form to login in utan function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,44 @@
-import React from "react";
-//import styled from "styled-components";
+import React, { useState } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-//import { Button } from "./Button";
+import { Button } from "./Button";
+
+//const LOGIN_URL = "http://localhost:8080/sessions";
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  a {
+    text-decoration: none;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  label {
+    margin: 10px;
+    color: #254b62;
+  }
+`;
 
 export const Login = () => {
-  return <div> här ska vara ett loggin form</div>;
+  const handleLogin = (event) => {};
+  return (
+    <Section>
+      <Form onSubmit={handleLogin}>
+        <label for="email">
+          Email
+          <input type="email" id="email" name="email" />
+        </label>
+        <label for="password">
+          Password
+          <input type="password" id="password" name="password" />
+        </label>
+
+        <Button type="submit" title="Login" />
+      </Form>
+    </Section>
+  );
 };
