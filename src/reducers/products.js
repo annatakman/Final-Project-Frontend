7e1cc51b92acaa7ab8a1d07d1ef60b7462c13544
annatakman <annatakman@gmail.com>
2020-06-03 19:02:10 +0200

    create product reducer and thunk to fetch products

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -2,5 +2,25 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const products = createSlice({
   name: 'products',
-  initialState: {}
+  initialState: [],
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload
+    },
+    return: () => {
+      return initialState
+    }
+  }
 })
+
+//Thunk to fetch products from our API
+export const fetchProducts = () => {
+  const PRODUCTS_URL = 'http://localhost:8080/products'
+  return (dispatch) => {
+    fetch(PRODUCTS_URL)
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(products.actions.setProducts(json))
+      })
+  }
+}
