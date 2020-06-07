cd235abf96f8bba7222e065196e8cfeec2e59710
annatakman <annatakman@gmail.com>
2020-06-07 14:51:01 +0200

    add to cart button on detail page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -5,8 +5,35 @@ import { Button } from 'components/Button'
 
 const DetailPage = styled.section``
 
+const Article = styled.article`
+  position: relative;
+  width: 70vh;
+  background: #d8dadc;
+`
 const Image = styled.img`
-height: 70vh;
+width: 100%;
+`
+
+const Details = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+`
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 75%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+
+  ${Article}:hover & {
+    opacity: 1;
+  }
 `
 
 export const ProductDetails = () => {
@@ -24,10 +51,17 @@ export const ProductDetails = () => {
 
   return (
     <DetailPage>
+      <Article>
       <Image src={product.imageUrl} alt={product.name}></Image>
+      <Details>
       <h4>{product.description}</h4>
       <p>Size: {product.size}</p>
       <p>Price: {product.price} EUR</p>
+      </Details> 
+      <ButtonWrapper>
+        <Button title='Add to cart' />
+      </ButtonWrapper>
+      </Article>
       <Link to='/products'>
         <Button title='Back to all products' />
       </Link>
