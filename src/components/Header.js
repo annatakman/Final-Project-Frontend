989c924eda22aff030c72a7c8ee0cf9496cd3610
Kajsa Bootin <kajsa@Kajsas-MacBook-Air.local>
2020-06-12 13:17:25 +0200

    added cart icon and total nbr of products

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { user } from '../reducers/user'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = styled.nav`
   display: flex;
@@ -50,6 +52,9 @@ const ListItem = styled.li`
 export const Header = () => {
   const dispatch = useDispatch()
   const accessToken = useSelector((store) => store.user.login.accessToken)
+  const cartItems = useSelector((store) => store.cart.items) //denna la jag till
+  const totalProducts = cartItems.length //denna la jag till
+
   const handleSignOut = () => {
     dispatch(user.actions.logout())
   }
@@ -62,7 +67,9 @@ export const Header = () => {
 
       <NavList>
         <Link to="/cart">
-          <ListItem>Cart</ListItem>
+          <ListItem>
+            <FontAwesomeIcon icon={faShoppingCart} /> ({totalProducts})
+          </ListItem>
         </Link>
 
         {!accessToken && (
